92b535db6e7441187e1de4cd18733371b0becaeb
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-09 00:25:58 -0400

    SwitchCases

--- src/Condicoes/EstudoSwitchCase.java
+++ src/Condicoes/EstudoSwitchCase.java
@@ -1,6 +1,48 @@
 package Condicoes;
 
+import java.util.Scanner;
+
 public class EstudoSwitchCase {
     static void main() {
+
+        /*
+        * SwitchCases: que servem para gerar casos especificos
+        * Objetivo: pedir pro usuario escolher entre os ninjas
+        * */
+
+
+        // Pedir para o usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // mostrar opções para o usuario
+        System.out.println("Escolha um personagem: ");
+        System.out.println("1 - Naruto");
+        System.out.println("2 - Zed");
+        System.out.println("3 - Akali");
+
+        // pedir para o usuario escolher uma opção
+        int escolhaDoUsuario = scanner.nextInt();
+
+        System.out.println("Você digitou o numero: " + escolhaDoUsuario);
+
+        // Reaçõa ao escolher um personagem
+
+        switch (escolhaDoUsuario) {
+            case 1:
+                System.out.println("O usuario escolheu o Naruto");
+                break;
+            case 2:
+                System.out.println("O usuario escolheu o Zed");
+                break;
+            case 3:
+                System.out.println("O usuario escolheu a Akali");
+                break;
+            default:
+                System.out.println("Você não digitou uma reposta valida, tente de novo!");
+        }
+
+        // Fechar a caixa
+        scanner.close();
+
     }
 }
