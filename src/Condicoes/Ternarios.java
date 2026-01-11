a8c285261bd6c74a98c34879e5d4bf2e6548fcc8
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-10 20:55:23 -0400

    Ternarios on java

---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,20 @@
+package Condicoes;
+
+public class Ternarios {
+    static void main() {
+
+
+        /*
+        * Ternarios: Sâo maneiras de reduzir o codigo
+        * variavel = (condição) ? valorSeVerdadeiro: valorFalso;
+        * */
+
+
+
+        short numeroDeMissoes = 9;
+        String nivelDoNinja = (numeroDeMissoes >= 10) ? "Esse Ninja esta com mais de 10 missões" : "Esse ninja tem menos de 10 missões";
+        System.out.println(nivelDoNinja);
+
+
+    }
+}
