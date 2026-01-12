fcb19718c58339efe553c4f1f7542df87827d84a
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-12 19:56:54 -0400

    Arrays

---
+++ src/studying/Arrays.java
@@ -0,0 +1,15 @@
+package studying;
+
+public class Arrays {
+    static void main() {
+
+        String[] ninja = new String[4];
+        ninja[0] = "Naruto";
+        ninja[1] = "Zed";
+        ninja[2] = "Akali";
+        ninja[3] = "Kakashi";
+
+        System.out.println(ninja[3]);
+
+    }
+}
