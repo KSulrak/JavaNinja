16dea9f886764ffba8718721e46248abc8ae6e71
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-13 22:59:09 -0400

    Array + desafio: cadastro de ninjas

--- src/studying/Arrays.java
+++ src/studying/Arrays.java
@@ -5,11 +5,27 @@ public class Arrays {
 
         // Arrays são do tipo referencia!
         // String inicializam com null
-        String[] ninja = new String[4];
+        String[] ninja = new String[5];
         ninja[0] = "Naruto";
         ninja[1] = "Zed";
         ninja[2] = "Akali";
         ninja[3] = "Kakashi";
+        System.out.println(ninja[4]);
+
+        // Redeclarar o array
+        ninja = new String[7];
+        ninja[0] = "Shen";
+        ninja[1] = "Tsunade";
+        ninja[2] = "Sasuke";
+        ninja[3] = "Naruto Uzumaki";
+        ninja[4] = "Gaara";
+        ninja[5] = "kakashi";
+        ninja[6] = "Sakura";
+
+        // For para fazer um loop no array
+        for (int i = 0; i < 7; i++) {
+            System.out.println(ninja[i]);
+        }
 
         // int inicializam com 0
         int[] idade = new int[2];
