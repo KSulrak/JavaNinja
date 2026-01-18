915bdc7e789da2d7bf707912c91f5c402e3c370a
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-17 22:03:06 -0400

    Desafios deepseek 1

---
+++ src/DesafiosDeepseek/desafio1.java
@@ -0,0 +1,24 @@
+package DesafiosDeepseek;
+
+public class desafio1 {
+    static void main() {
+
+
+        // Usando for para imprimiro do numero 0 ao 10
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+
+        }
+
+        System.out.println("Usando While");
+        System.out.println("---------------------------");
+
+        // Usando for para imprimiro do numero 10 ao 0
+        int numero = 11;
+
+        while (numero >= 1) {
+            numero--;
+            System.out.println(numero);
+        }
+    }
+}
