e0b48a0a1d1665e0939c82676d3b3cbe85c8cf8f
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-17 22:42:07 -0400

    Desafios deepseek 2

---
+++ src/DesafiosDeepseek/desafio2.java
@@ -0,0 +1,28 @@
+package DesafiosDeepseek;
+
+public class desafio2 {
+    static void main() {
+
+
+        // Somando os numeros de 1 a 100
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            soma = soma + i;
+
+        }
+        System.out.println(soma);
+
+        // Somandos os numeros pares de 1 a 50
+        int somaPar = 0;
+
+        for (int i = 0; i <= 50; i+= 2) {
+            somaPar += i;
+            
+        }
+        System.out.println(somaPar);
+
+
+
+    }
+}
