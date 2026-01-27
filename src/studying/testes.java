9a9eca0b4ffddae0cfd63754d13b809065fece19
Carlos Henrique <devcarlos.developer@gmail.com>
2026-01-26 23:37:04 -0400

    testes

---
+++ src/studying/testes.java
@@ -0,0 +1,13 @@
+package studying;
+
+public class testes {
+    static void main() {
+
+        String testes;
+
+        testes = "aqui tem um valor";
+
+        System.out.println(testes);
+
+    }
+}
