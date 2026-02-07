a0dc564f912b9bdfea9bf8c3d2c8308d18d5e59c
Carlos Henrique <devcarlos.developer@gmail.com>
2026-02-06 22:04:04 -0400

    duro correção

--- src/studying/teste3.java
+++ src/studying/teste3.java
@@ -3,9 +3,9 @@ package studying;
 public class teste3 {
     static void main() {
 
-        boolean duro = false;
+        boolean duro = true;
 
-        if (duro == true) {
+        if (duro) {
             System.out.println("Dorme");
         } else {
             System.out.println("Não dorme");
