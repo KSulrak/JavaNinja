29b2db98273e224335689475ab13370564477a7b
Carlos Henrique <devcarlos.developer@gmail.com>
2026-02-06 21:54:02 -0400

    duro

---
+++ src/studying/teste3.java
@@ -0,0 +1,15 @@
+package studying;
+
+public class teste3 {
+    static void main() {
+
+        boolean duro = false;
+
+        if (duro == true) {
+            System.out.println("Dorme");
+        } else {
+            System.out.println("Não dorme");
+        }
+
+    }
+}
