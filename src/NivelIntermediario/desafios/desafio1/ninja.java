0d9a1eededa6dd0da624a7d3edca4708b206541a
Carlos Henrique <devcarlos.developer@gmail.com>
2026-03-09 22:25:16 -0400

    primeiro desafio do nivel intermediario

--- src/NivelIntermediario/desafios/desafio1/ninja.java
+++ src/NivelIntermediario/desafios/desafio1/ninja.java
@@ -9,11 +9,11 @@ public class ninja {
 
 
         public void mostrarInformacoes() {
-            System.out.println(nome);
-            System.out.println(idade);
-            System.out.println(missao);
-            System.out.println(nivelDificuldade);
-            System.out.println(statusMissao);
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Missão: " + missao);
+            System.out.println("Nivel de dificuldade: " + nivelDificuldade);
+            System.out.println("Status da missão: " + statusMissao);
         }
 
     }
--- src/NivelIntermediario/desafios/desafio1/uchiha.java
+++ src/NivelIntermediario/desafios/desafio1/uchiha.java
@@ -8,9 +8,25 @@ public class uchiha extends ninja {
         System.out.println("A Habilidade especial " + habilidadeEspecial + " ativou!");
     }
 
+
+    /* versão com @Override
     @Override
     public void mostrarInformacoes() {
         super.mostrarInformacoes();
         mostrarHabilidade();
     }
+
+     */
+
+    public void mostrarInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Missão: " + missao);
+        System.out.println("Nivel de dificuldade: " + nivelDificuldade);
+        System.out.println("Status da missão: " + statusMissao);
+        System.out.println("Habilidade especial uchiha");
+
+    }
+
+
 }
