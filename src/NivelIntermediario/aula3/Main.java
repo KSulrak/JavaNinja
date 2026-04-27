e1840f1c5b79d105ebf5c2edd48f7d621404f069
Carlos Henrique <devcarlos.developer@gmail.com>
2026-04-27 00:54:27 -0400

    Construtor em Uzumaki e mudança no main

--- src/NivelIntermediario/aula3/Main.java
+++ src/NivelIntermediario/aula3/Main.java
@@ -3,49 +3,12 @@ package NivelIntermediario.aula3;
 public class Main {
     static void main() {
 
+        System.out.println("----------------Naruto Uzumaki----------------");
+        Uzumaki naruto = new Uzumaki("Naruto Uzumaki", "Aldeia da folha", 16, 30, NivelNinja.JOUNIN);
+        System.out.println(naruto);
 
 
-
-        // Obj Uzumaki
-        Uzumaki Naruto = new Uzumaki();
-        Naruto.nome = "Naruto Uzumaki";
-        Naruto.aldeia = "Aldeia da folha";
-        Naruto.idade = 16;
-        Naruto.habilidadeEspecial();
-        Naruto.EstrategiaDeBatalhaNinja();
-
-        // Obj Uchiha
-        Uchiha Sasuke = new Uchiha("Sasuke Uchiha", "Aldeia da folha", 18, 20, NivelNinja.CHUUNIN);
-        Sasuke.habilidadeEspecial();
-
-        // Obj Uchiha 2
-
-        Uchiha Itachi = new Uchiha("Itcha Uchiha", "Aldeia da folha", 27);
-        Itachi.habilidadeEspecial();
-        Itachi.metodoProvisorio();
-
-        // Obj hatake
-
-        Hatake kakashi = new Hatake();
-        kakashi.nome = "Kakashi Hatake";
-        kakashi.aldeia = "Aldeia da folha";
-        kakashi.idade = 48;
-        kakashi.boasVindas();
-        kakashi.shariganAtivado();
-        kakashi.ninjaDeElite();
-        kakashi.hokageAtivo();
-
-        // Obj Uchiha 3
-
-        Uchiha madara = new Uchiha("Madara", "Aldeia da folha", 45, 900, NivelNinja.KAGE);
-        madara.habilidadeEspecial();
-        madara.estrategiaDeBatalhaNinja();
-        // Metodo comum
-        madara.inteligenciaDeCombate();
-        // Metodo sobrecarregado
-        madara.inteligenciaDeCombate(90);
+        System.out.println("----------------Sasuke Uchiha----------------");
+        Uchiha sasuke = new Uchiha("Sasuke Uchiha", "Aldeia da folha", 18, 50, NivelNinja.JOUNIN);
     }
-
-
-
 }
--- src/NivelIntermediario/aula3/Uzumaki.java
+++ src/NivelIntermediario/aula3/Uzumaki.java
@@ -9,6 +9,15 @@ public class Uzumaki extends Ninja{
         System.out.println("Meu nome é: " + nome + " e esse é meu ataque especial");
     }
 
+    public Uzumaki() {
+    }
 
+    public Uzumaki(String nome, String aldeia, int idade) {
+        super(nome, aldeia, idade);
+    }
+
+    public Uzumaki(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, NivelNinja rank) {
+        super(nome, aldeia, idade, numeroDeMissoesConcluidas, rank);
+    }
 
 }
